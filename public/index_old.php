80aea8f77924cff08c9193271f847e0fbd4e1a57
MT.Chen <gene31105@gmail.com>
2018-07-07 03:47:21 +0800

    mirate to slim

---
+++ public/index_old.php
@@ -0,0 +1,33 @@
+<?php
+
+include_once(__DIR__ . '/../vendor/autoload.php');
+
+//setup whoops error handler
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
+
+
+$router = new \Klein\Klein();
+
+//set layout page
+$router->respond(function ($request, $response, $service) {
+    $service->layout('views/layouts/default.php');
+});
+
+$router->respond('/', function ($request, $response, $service) {
+    $service->render('views/home.php');
+});
+
+$router->respond('GET', '/login', function ($request, $response, $service) {
+    $service->render('views/auth/login.php');
+});
+$router->respond('POST', '/login', function ($request, $response, $service) {
+    $send = $request->paramsPost('name','password','remember');
+    var_dump($send);
+});
+
+
+
+$router->dispatch();
