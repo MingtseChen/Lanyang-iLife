932503e25416fb393348fcb53fcf4c25f9e6d361
MT.Chen <gene31105@gmail.com>
2018-07-27 22:23:10 +0800

    update get header method

--- src/Middleware/SSO.php
+++ src/Middleware/SSO.php
@@ -5,23 +5,21 @@ include __DIR__ . "/../auth.php";
 class SSO
 {
     private $session;
-    private $auth;
+    private $header;
 
     public function __construct()
     {
         $this->session = new \SlimSession\Helper;
-        $this->auth = new SSOAuth();
+        $this->header = apache_request_headers();
     }
 
     public function __invoke($request, $response, $next)
     {
-        $ssoLoginCheck = $this->auth->isSsoLogin();
-        $uri = $request->getUri();
-        $host = $uri->getHost();
-        var_dump($host);
-        if ($host = 'sso.tku.edu.tw') {
-            $userId = $request->getHeader('sso_userid');
-            $this->session->set('id', $userId);
+        $headers = apache_request_headers();
+        $ssoLogin = array_key_exists("sso_userid", $headers);
+        if ($ssoLogin) {
+            $this->session->set('id', $headers['sso_userid']);
+            $this->session->set('role', $headers['sso_roletype']);
         }
         $response = $next($request, $response);
         return $response;
