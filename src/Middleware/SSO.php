5ae3d57f1079b791afa75d77d31ec308f0e50a13
MT.Chen <gene31105@gmail.com>
2018-07-27 22:38:19 +0800

    Fix session set error

--- src/Middleware/SSO.php
+++ src/Middleware/SSO.php
@@ -4,24 +4,18 @@ include __DIR__ . "/../auth.php";
 
 class SSO
 {
-    private $session;
-    private $header;
-
-    public function __construct()
-    {
-        $this->session = new \SlimSession\Helper;
-        $this->header = apache_request_headers();
-    }
-
     public function __invoke($request, $response, $next)
     {
+        $session = new \SlimSession\Helper;
         $headers = apache_request_headers();
         $ssoLogin = array_key_exists("sso_userid", $headers);
-        if ($ssoLogin) {
-            $this->session->set('id', $headers['sso_userid']);
-            $this->session->set('role', $headers['sso_roletype']);
-        }
+
         $response = $next($request, $response);
+
+        if ($ssoLogin) {
+            $session->set('id', $headers['sso_userid']);
+            $session->set('role', $headers['sso_roletype']);
+        }
         return $response;
     }
 }
